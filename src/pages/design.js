8bda2a8f1fd03e0de39c8efb65e7111cfa254db7
jrcolby <j.r.colby@gmail.com>
2020-10-11 14:28:25 -0700

    deleted extraneous pages

--- src/pages/design.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Container } from "../components/Container"
-import {Content} from "../components"
-export default function Design(){
-    return <Container>
-    <Content></Content>
-  </Container>
-}
--- src/pages/music.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Container } from "../components/Container"
-import {Content} from "../components"
-
-export default function Music(){
-    return <Container>
-    <Content>HEEEY</Content>
-  </Container>
-}
